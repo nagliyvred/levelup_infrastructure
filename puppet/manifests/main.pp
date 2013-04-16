d58dca8f8a3485bf7ea7de8d29561fa5ae44e009
Evgeny Dudin <edudin@edudin.(none)>
2013-04-16 20:43:21 +1000

    evgeny - added a testfile

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -13,7 +13,19 @@ node "master.levelup" inherits default {
 #  include puppetmaster
   include mcollective::client
   include active_mq
-
-
 }
+
+
+ node "possum.levelup" inherits default {
+
+    file { "/etc/testfile":
+      ensure => present,
+      owner => "root"
+    }
+
+ }
+
+
+
+
 
